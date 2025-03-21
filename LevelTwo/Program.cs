75639a9cccf60825cd7f4f1930e6b1c686cb28d9
DinisB <dinisbarroso.dev@gmail.com>
2025-03-21 12:25:53 +0000

    Final commit

---
+++ LevelTwo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Spectre.Console;
+using SoGoodLib;
+public static class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(SoGoodClass.SoGoodMethod());
+        string pasta = "tux.jpg";
+        int tamanho = 20;
+        //Set the max width of the image.
+        if (args.Length >= 2)
+        {
+            pasta = args[0];
+
+            if (!int.TryParse(args[1], out tamanho) || tamanho <= 0)
+            {
+                tamanho = 20;
+            }
+        }
+        //Render the image to the console
+        CanvasImage image = new CanvasImage(pasta);
+        image.MaxWidth(tamanho);
+        //Render the image to the console
+        AnsiConsole.Write(image);
+    }
+}
---
+++ SoGoodLib/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SoGoodLib
+{
+    public class Class1
+    {
+
+    }
+
+    public static class SoGoodClass
+    {
+        public static string SoGoodMethod()
+        {
+            return "I came from another project!";
+        }
+    }
+}
