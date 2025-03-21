d46fde827cb3d431d9bd4361b4c2775540d00851
DinisB <dinisbarroso.dev@gmail.com>
2025-03-21 14:50:57 +0000

    WorkerTitle

---
+++ WorkerTable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Spectre.Console;
+using Bogus;
+
+namespace WorkerTable
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Table tabela;
+            Randomizer.Seed = new Random(int.Parse(args[0]));
+            tabela = new Table();
+            Faker faker = new Faker("pt_PT");
+            tabela.AddColumn("ID");
+            tabela.AddColumn("Name");
+            tabela.AddColumn("Job");
+            int count = int.Parse(args[0]);
+            for (int i = 0; i < count; i++)
+            {
+                int fakerid = i;
+                string randomName = faker.Parse("{{name.lastName}}, {{name.firstName}} {{name.suffix}}");
+                string randomJob = faker.Parse("{name.fullJob}");
+
+            tabela.AddRow(fakerid, randomName, randomJob);
+            }
+            AnsiConsole.Write(tabela);
+        }
+    }
+}
